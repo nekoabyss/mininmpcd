ef0d1c91f637ef5e39a417b6ede7f0ca4736922e
nekoabyss <neko.abyss@gmail.com>
2018-10-16 17:14:02 +0700

    edit GET ; added disName for query

--- src/index.php
+++ src/index.php
@@ -8,18 +8,11 @@ error_reporting(0);
 $response = [
     'query' => $_GET,
     'name' => $tab_name,
+    //'id' => 0,
     'total' => 0,
     'results' => array(),
     'response' => ['status' => 'failed'],
 ];
-
-
-// get and prepare params
-//$response['query'] = $_GET;
-
-$params = [];
-if (isset($_GET['distributorName'])) $params[] = "distributorName LIKE %" . $_GET['distributorName'] . "%";
-if (isset($_GET['distributorId'])) $params[] = "distributor_id = " . $_GET['distributorId'];
 
 // get query result, encode as json, and print
 try {
@@ -28,12 +21,19 @@ try {
         throw new Exception($db->connect_error);
     }
 
+    // get and prepare params
+    //$response['query'] = $_GET;
+
+    $params = [];
+    if (isset($_GET['distributorName'])) $params[] = "distributor_name LIKE '%" . $db->escape_string($_GET['distributorName']) . "%'";
+    if (isset($_GET['distributorId'])) $params[] = "distributor_id = " . $db->escape_string($_GET['distributorId']);
+
     // prepare and execute query statement
     $sql = "SELECT tab_name FROM distributor";
     if (sizeof($params) > 0) {
         $sql .= " WHERE " . implode(" AND ", $params);
     }
-    $sql = $db->escape_string($sql);
+    //echo $sql;
 
     if ($tab = $db->query($sql)) {
         $tab_name = $tab->fetch_assoc()['tab_name'];
@@ -41,12 +41,21 @@ try {
         $sql = "SELECT * FROM $tab_name";
         $sql = $db->escape_string($sql);
 
+//        $sql2 = "SELECT distributor_id FROM distributor WHERE tab_name LIKE $tab_name";
+  //      $sql2 = $db->escape_string($sql2);
+    //    $sql2_result = $db->query($sql2);
+      //  $id = $sql2_result->fetch_assoc();
+
+
+        echo $sql2;
+
         if ($query_result = $db->query($sql)) {
             while ($item = $query_result->fetch_assoc()) {
                 $response['results'][] = $item;
 
             }
             $response['name'] = $tab_name;
+            //$response['id'] = $id;
             $response['total'] = sizeof($response['results']);
             $response['response']['status'] = 'success';
         }
@@ -59,5 +68,7 @@ try {
     // close db connection
     $db->close();
 }
+
+
 
 echo json_encode($response, JSON_PRETTY_PRINT);
